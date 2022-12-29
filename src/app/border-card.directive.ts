a5106079e68129503cb894c5e14accc5fc6a6475
thibautDevWorld <thibaut.lartigue.pro@gmail.com>
2022-12-29 18:38:29 +0100

    add directives attributes on card when mouseover

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -5,19 +5,24 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 
 export class BorderCardDirective {
+
+    private initialColor: string = '#f5f5f5';
+    private defaultColor: string = '#009688';
+    private defaultHeight: number = 180;
+
     constructor(private el: ElementRef) {
-        this.setBorder('#f5f5f5');
-        this.setHeight(180)
+        this.setBorder(this.initialColor);
+        this.setHeight(this.defaultHeight)
     }
 
     @Input('pkmnBorderCard') borderColor: string;
 
     @HostListener('mouseenter') onMouseEnter() {
-        this.setBorder(this.borderColor || '#009688');
+        this.setBorder(this.borderColor || this.defaultColor);
     }
 
     @HostListener('mouseleave') onMouseLeave() {
-        this.setBorder('#f5f5f5');
+        this.setBorder(this.initialColor);
     }
 
     private setBorder(color: string) {
