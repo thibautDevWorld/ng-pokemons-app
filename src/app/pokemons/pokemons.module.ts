06ab3f0d5c341a4a46ba31c580d886566ef3b8a6
thibautDevWorld <thibaut.lartigue.pro@gmail.com>
2022-12-29 22:18:39 +0100

    push all pokemon features inside a child module

---
+++ src/app/pokemons/pokemons.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core'; 
+import { CommonModule } from '@angular/common'; 
+ 
+import { ListPokemonComponent } from './list-pokemon.component'; 
+import { DetailPokemonComponent } from './detail-pokemon.component'; 
+import { BorderCardDirective } from './border-card.directive';  
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe'; 
+import { PokemonRoutingModule } from './pokemons-routing.module';
+ 
+@NgModule({ 
+ imports: [ 
+  CommonModule,
+  PokemonRoutingModule
+ ], 
+ declarations: [ 
+  ListPokemonComponent, 
+  DetailPokemonComponent, 
+  BorderCardDirective,
+  PokemonTypeColorPipe
+ ]
+}) 
+export class PokemonsModule {}
