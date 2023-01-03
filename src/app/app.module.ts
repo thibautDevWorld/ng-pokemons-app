048aad6d348754a44ca422157d32454fa3e978b1
thibautDevWorld <thibaut.lartigue.pro@gmail.com>
2023-01-03 10:32:41 +0100

    add authentication with guard service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,20 @@ import { PokemonsModule } from './pokemons/pokemons.module';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    LoginRoutingModule,
     PokemonsModule,
     AppRoutingModule,
     HttpClientModule,
